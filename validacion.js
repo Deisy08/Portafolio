c8c0a437328909a4fe9aa9f398b0551e6214eea9
Deisy <trujillosanchezgianina@gmail.com>
2023-03-10 20:37:55 -0500

    actualizado hasta experiencia

--- validacion.js
+++ validacion.js
@@ -1,26 +1,61 @@
-function validarDatos(){
-    window.event.preventDefault()
-     
-    if (document.form.nombre.value=="" ) {
-        alert("Campo nombre es obligatorio")  
-        document.form.nombre.focus()
-    
-    }else if (document.form.email.value=="") {
-        alert("Campo e-mail es obligatorio")
-        document.form.email.focus() 
-
-    }else if (document.form.asunto.value=="" ) {
-        alert("Campo Asunto es obligatorio")
-        document.form.asunto.focus() 
-
-    }else if (document.form.mensaje.value=="" || document.form.mensaje.value.length <= 50 ){
-        alert("Campo Mensaje es obligatorio y debe contener máximo 50 carateres") 
-        document.form.mensaje.focus()
-      
-    } else if (document.form.email.value.indexOf('@')==-1 ||
-    document.form.email.value.indexOf('.')==-1 ) {
-        alert("e-mail inválido")
+function valida(input) {
+    const tipoDeInput = input.dataset.tipo;
+    if (validadores[tipoDeInput]) {
+      validadores[tipoDeInput](input);
     }
-   
+  
+    if (input.validity.valid) {
+      input.parentElement.classList.remove("input-container--invalid");
+      input.parentElement.querySelector(".input-message-error").innerHTML = "";
+    } else {
+      input.parentElement.classList.add("input-container--invalid");
+      input.parentElement.querySelector(".input-message-error").innerHTML =
+        mostrarMensajeDeError(tipoDeInput, input);
+    }
 }
-    document.querySelector('form').addEventListener('submit',validarDatos)
+  
+const tipoDeErrores = [
+    "valueMissing",
+    "typeMismatch",
+    "patternMismatch",
+    "customError",
+];
+  
+const mensajesDeError = {
+    nombre: {
+      valueMissing: "El campo nombre no puede estar vacío",
+    },
+    email: {
+      valueMissing: "El campo correo no puede estar vacío",
+      typeMismatch: "El correo no es válido",
+    },
+    asunto: {
+      valueMissing: "Este campo no puede estar vacío",
+      patternMismatch: "Este campo puede contener hasta 20 caracteres.",
+    },
+    mensaje: {
+        valueMissing: "Este campo no puede estar vacío",
+        patternMismatch: "El campo mensaje puede contener desde 10 hasta 50 caracteres.", 
+    }
+};
+  
+function mostrarMensajeDeError(tipoDeInput, input) {
+    let mensaje = "";
+    tipoDeErrores.forEach((error) => {
+      if (input.validity[error]) {
+        console.log(tipoDeInput, error);
+        console.log(input.validity[error]);
+        console.log(mensajesDeError[tipoDeInput][error]);
+        mensaje = mensajesDeError[tipoDeInput][error];
+      }
+    });
+    return mensaje;
+}
+
+const inputs = document.querySelectorAll('input');
+
+inputs.forEach((input) => {
+   input.addEventListener('blur', (input) => {
+    valida(input.target)
+   })
+})
